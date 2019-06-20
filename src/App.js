c1b85ee22024b89835e02079457712e955659fc4
Norberto <caceresnorbertoenrique@gmail.com>
2019-06-19 20:00:34 -0500

    add color helpers to dynamically generate color scale

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import seedPalettes from './seedPalettes'
 // components
 import Palette from './Palette/Palette'
 import './App.css'
+import { generatePalette } from './colorHelpers'
 
 function App() {
+	console.log(generatePalette(seedPalettes[0]))
+
 	return (
 		<div className='App'>
 			<Palette {...seedPalettes[1]} />
